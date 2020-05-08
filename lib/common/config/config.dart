25dc2c2568606075166a90e6702b1b373e738308
shiminghua <shiminghua2008@qq.com>
2020-05-08 10:37:34 +0800

    config

---
+++ lib/common/config/config.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class Config {
+  static const PAGE_SIZE = 10;
+
+  static const BASE_URL = 'http://127.0.0.1:3000';
+
+  static const GLOBAL_COLOR = Color.fromARGB(255, 0, 127, 255);
+}
