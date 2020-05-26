bb0aa0b1a860cfa25cfae8042d648b7435f291d7
shiminghua <shiminghua2008@qq.com>
2020-05-26 10:29:16 +0800

    counter

--- lib/widgets/job/job_item.dart
+++ lib/widgets/job/job_item.dart
@@ -2,16 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_boss/common/config/config.dart';
 import 'package:flutter_boss/model/job.dart';
 
-// class JobItem extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Row(
-//       children: <Widget>[
-//         new Text('job_item'),
-//       ],
-//     );
-//   }
-// }
+
 class JobItem extends StatelessWidget {
   final Job job;
   VoidCallback onPressed;
--- lib/widgets/job_page.dart
+++ lib/widgets/job_page.dart
@@ -25,22 +25,47 @@ class JobPage extends StatefulWidget {
 
 class _JobPageState extends State<JobPage> with AutomaticKeepAliveClientMixin {
   Future<List<Job>> _fetchJobList() async {
-    print(333333);
-    List<Job> jobList = List<Job>();
-    print(4444);
-    print(jobList);
-    Response<Map<String, dynamic>> response =
-        await HttpUtil().get('/jobs/list/1/');
-    print(11111);
-    print(response);
-    if (response.statusCode == 200) {
-      Map<String, dynamic> result = response.data;
-      for (dynamic data in result['data']['jobs']) {
-        Job jobData = Job.fromJson(data);
-        jobList.add(jobData);
-      }
-    }
-    return jobList;
+    return Future.delayed(
+      new Duration(seconds: 2),
+      () => [
+        new Job.fromJson({
+          'id': '111',
+          'title': '222222',
+          'salary': '33333',
+          'company': '44444',
+          'info': '55555',
+          'category': '66666',
+          'head': '77777',
+          'publish': '88888',
+        }),
+        new Job.fromJson({
+          'id': '111',
+          'title': '222222',
+          'salary': '33333',
+          'company': '44444',
+          'info': '55555',
+          'category': '66666',
+          'head': '77777',
+          'publish': '88888',
+        }),
+      ],
+    );
+    // print(333333);
+    // List<Job> jobList = List<Job>();
+    // print(4444);
+    // print(jobList);
+    // Response<Map<String, dynamic>> response =
+    //     await HttpUtil().get('/jobs/list/1/');
+    // print(11111);
+    // print(response);
+    // if (response.statusCode == 200) {
+    //   Map<String, dynamic> result = response.data;
+    //   for (dynamic data in result['data']['jobs']) {
+    //     Job jobData = Job.fromJson(data);
+    //     jobList.add(jobData);
+    //   }
+    // }
+    // return jobList;
   }
 
   @override
@@ -61,15 +86,19 @@ class _JobPageState extends State<JobPage> with AutomaticKeepAliveClientMixin {
         child: new FutureBuilder(
           future: _fetchJobList(),
           builder: (context, AsyncSnapshot snapshot) {
-            print(222222);
+            print(snapshot.connectionState);
+            print(11111111111);
+            print(snapshot.data.toString());
             switch (snapshot.connectionState) {
               case ConnectionState.none:
               case ConnectionState.waiting:
                 return CircularProgressIndicator();
               default:
-                if (snapshot.error) {
+                if (snapshot.hasError) {
+                  print(3333333);
                   return new Text('Error: ${snapshot.error}');
                 } else {
+                  print(222222222);
                   return _createListView(context, snapshot);
                 }
             }
